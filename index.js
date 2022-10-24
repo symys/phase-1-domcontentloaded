d0f8056d272e83c1f5d4d89f95e6452295db5c42
SeymaDemir <seymademir35@gmail.com>
2022-10-24 11:40:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const changeText = document.getElementById("text");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    changeText.textContent = "This is really cool!"
+  });
